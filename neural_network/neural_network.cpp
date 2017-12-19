c31f0842eae50a8bea7775e67d9c9bfc16b7eec4
Karel van Waveren <KarelJF@gmail.com>
2017-12-19 13:40:57 +0100

    added loads of cats and dogs

--- neural_network/neural_network.cpp
+++ neural_network/neural_network.cpp
@@ -3,16 +3,40 @@
 #include <opencv2/features2d/features2d.hpp>
 #include "neural_network.h"
 #include <opencv2/highgui.hpp>
+#include <iostream>
+#include <experimental/filesystem>
 
+typedef vector<string>::const_iterator myvector;
+namespace fs = std::experimental::filesystem::v1;
 
-Mat neural_network::get_descriptors(Mat img, Mat & descriptors)
+Mat neural_network::get_descriptors(Mat img)
 {
+	Mat descriptors;
 	Ptr<ORB> orb = ORB::create();
 	Mat mask;
 	vector<KeyPoint> kp = vector<KeyPoint>();
 	orb->detectAndCompute(img, mask, kp, descriptors);
-	Mat img2;
-	drawKeypoints(img, kp, img2);
-	imshow("aardbei", img2);
 	return descriptors;
+}
+
+void neural_network::read_files(string pathImages, vector<string> & files) {
+	for (auto & p : fs::directory_iterator(pathImages)) {
+		files.push_back(p.path().string());
+	}
+}
+
+void neural_network::read_images(myvector begin, myvector end, std::function<void(const std::string&, const cv::Mat&)> callback) {
+	for (auto it = begin; it != end; it++) {
+		string readfilename = *it;
+		// Imread 1 --> color // Imread 0 --> grayscale
+		Mat image = imread(readfilename, 1);
+		if (image.empty()) {
+			cout << "The image is empty! " << endl;
+		}
+		else {
+			string classname = readfilename.substr(readfilename.find_last_of("\\") + 1, 3);
+			Mat descriptors = this->get_descriptors(image);
+			callback(classname, descriptors);
+		}
+	}
 }
--- neural_network/neural_network.h
+++ neural_network/neural_network.h
@@ -6,5 +6,15 @@ using namespace cv;
 class neural_network
 {
 public:
-	Mat neural_network::get_descriptors(Mat img, Mat & descriptors);
-};
+	Mat neural_network::get_descriptors(Mat img);
+	// func: Reads images from filenames
+	// 
+	typedef vector<string>::const_iterator myvector;
+	void neural_network::read_images(myvector begin, myvector end, std::function<void(const std::string&, const cv::Mat&)> callback);
+	void neural_network::read_files(string pathImages, vector<string> & files);
+	};
+	struct image_data
+	{
+		std::string class_name;
+		cv::Mat bow_features;
+	};
